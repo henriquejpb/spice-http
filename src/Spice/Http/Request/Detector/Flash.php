189f215b11e84d7b2b9fb0c249bdcc61cdba3cfa
Henrique Barcelos <rick.hjpbarcelos@gmail.com>
2013-12-02 01:05:47 -0200

    Adds flash detector and its test

---
+++ src/Spice/Http/Request/Detector/Flash.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * A method detector will verify the HTTP request was originated in a flash object.
+ *
+ * @package Spice\Http\Request\Detector;
+ */
+namespace Spice\Http\Request\Detector;
+
+use Spice\Http\Request\RequestInterface;
+
+/**
+ * Detector for flash requests.
+ */
+class Flash implements DetectorInterface {
+    /**
+     * Returns `true` if the request is originated in a flash object or `false` otherwise.
+     *
+     * @inherit-doc
+     */
+    public function matches(RequestInterface $request) {
+        return (bool) preg_match('/shockwave ?flash/i', $request->getServer('HTTP_USER_AGENT', false));
+    }
+}
---
+++ test/unit/Spice/Http/Request/Detector/FlashTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace Spice\Http\Request\Detector;
+
+use Spice\Http\Request\RequestInterface;
+
+class FlashTest extends AbstractDetectorTest {
+    private function getDetector($method) {
+        return new Flash($method);
+    }
+
+    /**
+     * @testdox Method `matches` returns true when request `HTTP_USER_AGENT` param contains 'shockwave|flash'
+     * @test
+     */
+    public function testMatchesReturnsTrueWhenHttpUserAgentContainsKeywords() {
+        $detector = $this->getDetector('post');
+        $request = $this->getRequestMock('post');
+        $request->expects($this->any())
+            ->method('getServer')
+            ->with($this->equalTo('HTTP_USER_AGENT'))
+            ->will($this->returnValue('shockwave flash'));
+        $this->assertTrue($detector->matches($request));
+    }
+
+    /**
+     * @testdox Method `matches` returns true when request `HTTP_USER_AGENT` param does not contain 'shockwave|flash'
+     * @test
+     */
+    public function testMatchesReturnsFalseWhenHttpUserAgentDoesNotContainKeywords() {
+        $detector = $this->getDetector('post');
+        $request = $this->getRequestMock('post');
+        $request = $this->getRequestMock('post');
+        $request->expects($this->any())
+            ->method('getServer')
+            ->with($this->equalTo('HTTP_USER_AGENT'))
+            ->will($this->returnValue('mozilla'));
+        $this->assertFalse($detector->matches($request));
+    }
+
+}
